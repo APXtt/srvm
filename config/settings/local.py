bc9a739ca5d82fbac589af9221ca2e61347ddbb2
안현우 <dfwtt11@gmail.com>
2022-11-14 03:41:07 +0900

    Write srvm models Complete

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,1 @@
 from .base import *
-
-# STATICFILES_DIRS = []
--- service/models.py
+++ service/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=200)
+    img_url = models.TextField()
+    create_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    rental_cost = models.IntegerField()
+
+
+class user(models.Model):
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    point = models.IntegerField()
